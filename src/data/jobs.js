88f283ac1ae9df48214776b8dddc04fd58670f95
Pablo Suárez <104940521+pabloSuarezDev@users.noreply.github.com>
2023-02-26 21:23:37 +0000

    Add files via upload

---
+++ src/data/jobs.js
@@ -0,0 +1,36 @@
+export const jobs = [
+  {
+    'id': 'countries',
+    'name': 'Buscador de Países',
+    'url': 'https://github.com/pabloSuarezDev/Countries-searcher',
+    'technologies': 'React - SASS - BootStrap5',
+    'categories': 'Desarrollo Frontend'
+  },
+  {
+    'id': 'redsocial',
+    'name': 'Red Social',
+    'url': 'https://github.com/pabloSuarezDev/social-network',
+    'technologies': 'Laravel, MySQL',
+    'categories': 'Desarrollo FullStack'
+  },
+  {
+    'id': 'miblog',
+    'name': 'MiBlog (MERN STACK)',
+    'url': 'https://github.com/pabloSuarezDev/MiBlog-MERN-STACK',
+    'technologies': 'React, Node, Express, MongoDB - Mongoose',
+    'categories': 'Desarrollo FullStack'
+  },
+  {
+    'id': 'ecommerce',
+    'name': 'Tienda Online',
+    'url': 'https://github.com/pabloSuarezDev/e-commerce',
+    'technologies': 'HTML5, CSS3, PHP(POO, MVC), MySQL',
+    'categories': 'Desarrollo FullStack'
+  }, {
+    'id': 'hardware',
+    'name': 'Almacén de Hardware (móvil)',
+    'url': 'https://github.com/pabloSuarezDev/Hardware-Warehouse-App',
+    'technologies': 'React, Ionic, Node, Express, MySQL - Sequelize',
+    'categories': 'Desarrollo FullStack'
+  }
+];
